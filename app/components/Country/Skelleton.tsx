b46a651e45f5f80aca2ffbd83cc3a7f2ab06562d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-11-10 14:14:54 -0300

    feat: adiciona pequena animação no skelleton

--- app/components/Country/Skelleton.tsx
+++ app/components/Country/Skelleton.tsx
@@ -8,7 +8,7 @@ export default function Skelleton({ cardsQuantity = 3 }: Props) {
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
       {arrayToMap.map((_, index) => (
-        <li key={index} className="bg-white rounded-xl px-2 py-3 pb-0 h-full block shadow-sm hover:shadow-md">
+        <li key={index} className="animate-pulse bg-white rounded-xl px-2 py-3 pb-0 h-full block shadow-sm hover:shadow-md">
           <div className="rounded-md bg-gray-300 aspect-video object-cover"></div>
           <div className="bg-gray-300 h-5 my-3"></div>
         </li>

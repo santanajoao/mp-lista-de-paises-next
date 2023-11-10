541b7a48ea9dd9c86e420804aebe404c816d4ba3
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-11-10 01:51:55 -0300

    chore: cria funções de requisição dos detalhes de um país

--- app/api/countriesApi.ts
+++ app/api/countriesApi.ts
@@ -1,4 +1,4 @@
-import type { CountryListItem } from "~/types/country";
+import type { CountryDetails, CountryDetailsResponse, CountryListItem } from "~/types/country";
 
 const apiUrl = 'https://restcountries.com/v3.1';
 
@@ -6,4 +6,10 @@ export const getAllCountries = async (): Promise<CountryListItem[]> => {
   const response = await fetch(`${apiUrl}/all?fields=cca3,translations,flags`);
   const countryList: CountryListItem[] = await response.json();
   return countryList;
+};
+export const getCountryDetails = async (countryCode: string): Promise<CountryDetails> => {
+  const response = await fetch(`${apiUrl}/alpha/${countryCode}?fields=translations,flags,capital,languages,population,borders,continents`);
+  const data: CountryDetailsResponse = await response.json();
+  const country = { ...data, languages: Object.values(data.languages) };
+  return country;
 };

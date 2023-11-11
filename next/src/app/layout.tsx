b4f9d58a379bcdf61e0e6a616166cf0b1e304b4d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-11-11 00:19:23 -0300

    build: initial commit

---
+++ next/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Nunito_Sans } from 'next/font/google'
+import './globals.css'
+
+const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Lista de países',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={nunitoSans.className}>{children}</body>
+    </html>
+  )
+}

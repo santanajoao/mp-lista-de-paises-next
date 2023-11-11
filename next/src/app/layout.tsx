c42a29fd75f919eeaa2f18d613c87c8f9ef0959e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-11-11 00:27:35 -0300

    feat: adiciona header

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Nunito_Sans } from 'next/font/google';
 import './globals.css';
+import Image from 'next/image';
+import Header from '@/components/Header';
 
 const nunitoSans = Nunito_Sans({ subsets: ['latin'] });
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="pt-BR">
-			<body className={nunitoSans.className}>{children}</body>
+			<body className={`${nunitoSans.className} text-gray-800 bg-gray-100 min-h-screen flex flex-col`}>
+				<Header />
+
+				{children}
+			</body>
 		</html>
 	);
 }
---
+++ next/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+export default function Header() {
+	return (
+		<header className="bg-white">
+			<div className="m-auto max-w-[1366px] flex items-center p-2 sm:py-3 sm:px-6">
+				<Image
+					height={40}
+					width={40}
+					className="h-10 aspect-square mr-2"
+					src="/world-logo.svg"
+					alt="Logo da página. Representação simplificada do globo terrestre."
+				/>
+
+				<span className="text-2xl font-bold">Países do mundo</span>
+			</div>
+		</header>
+	);
+}
